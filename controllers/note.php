b8fb731e1f483867162a4ca8d7d690c0dbdbce07
abc <mnasr0868@gmail.com>
2023-08-31 00:02:59 +0300

    vid 23

--- controllers/note.php
+++ controllers/note.php
@@ -12,5 +12,19 @@ $query ="SELECT * FROM notes WHERE id = :id";
 
 $note =$db->Query($query,['id'=>$_GET['id']])->fetch();
 
+$currentUser =1;
+
+if(!$note){
+
+    abourt(Response::NOT_FOUND);
+
+}
+elseif ($note['user_id'] !== $currentUser)
+{
+
+    abourt(Response::FORBIDDEN);
+
+}
+
 
 require "views/note.view.php";

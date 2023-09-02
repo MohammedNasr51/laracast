12d0b14ef96ac7f0d1f7b177ed439252c5ea900b
abc <mnasr0868@gmail.com>
2023-09-03 00:21:09 +0300

    vid 32

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -10,17 +10,39 @@ $db = new Database($config['database']);
 
 $currentUser =1;
 
-$query ="SELECT * FROM notes WHERE id = :id";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $query ="SELECT * FROM notes WHERE id = :id";
 
 $note =$db->Query($query,['id'=>$_GET['id']])->FindOrFail();
 
 
 authorize($note['user_id'] === $currentUser);
 
+$db->Query('DELETE FROM notes where id = ?',[$_POST['id']]);
 
-view("notes/show.view.php", [
+header('Location: /laracast/notes');
 
-    'heading' => 'Note',
+exit();
+    
+}else
+{
 
-    'note' => $note
-]);
+    $query ="SELECT * FROM notes WHERE id = :id";
+
+    $note =$db->Query($query,['id'=>$_GET['id']])->FindOrFail();
+    
+    
+    authorize($note['user_id'] === $currentUser);
+
+    view("notes/show.view.php", [
+
+        'heading' => 'Note',
+    
+        'note' => $note
+    ]);
+}
+
+
+
+
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -5,13 +5,18 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
 
-    <p class ="mb-6">
-        <a href="/laracast/notes" class="text-blue-500 underline ">Go back...</a>
-    </p>
+        <p class="mb-6">
+            <a href="/laracast/notes" class="text-blue-500 underline ">Go back...</a>
+        </p>
         <p>
-            <?= htmlspecialchars($note['body'])  ?>
+            <?= htmlspecialchars($note['body']) ?>
         </p>
 
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
